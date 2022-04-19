8c79df72f3494b54d4a33c6dfa6ff17339b864ff
maticamisay <maticamisay99@hotmail.com>
2022-04-19 15:50:42 -0300

    cambio de api

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -12,45 +12,43 @@ const NoticiasProvider = ({ children }) => {
 
   useEffect(() => {
     const consultarAPI = async () => {
-      const url = `https://newsapi.org/v2/top-headlines?pageSize=9&country=ar&category=${categoria}&apiKey=${
-        import.meta.env.VITE_API_KEY
-      }`;
-
+      const url = `http://api.mediastack.com/v1/news?access_key=3120efc858f8a846d907c8e3966e2f29
+      `;
       const { data } = await axios(url);
-      setNoticias(data.articles);
-      setTotalNoticias(data.totalResults);
+      setNoticias(data.data);
+      setTotalNoticias(data.pagination.total);
       setPagina(1);
     };
     consultarAPI();
   }, [categoria]);
 
-  useEffect(() => {
-    const consultarAPI = async () => {
-      const url = `https://newsapi.org/v2/top-headlines?country=ar&pageSize=9&page=${pagina}&category=${categoria}&apiKey=${
-        import.meta.env.VITE_API_KEY
-      }`;
+  // useEffect(() => {
+  //   const consultarAPI = async () => {
+  //     const url = `https://newsapi.org/v2/top-headlines?country=ar&pageSize=9&page=${pagina}&category=${categoria}&apiKey=${
+  //       import.meta.env.VITE_API_KEY
+  //     }`;
 
-      const { data } = await axios(url);
-      setNoticias(data.articles);
-      setTotalNoticias(data.totalResults);
-    };
-    consultarAPI();
-  }, [pagina]);
+  //     const { data } = await axios(url);
+  //     setNoticias(data.articles);
+  //     setTotalNoticias(data.totalResults);
+  //   };
+  //   consultarAPI();
+  // }, [pagina]);
 
-  useEffect(() => {
-    const consultarAPI = async () => {
-      const url = `https://newsapi.org/v2/everything?q=${busqueda}&pageSize=9&apiKey=${
-        import.meta.env.VITE_API_KEY
-      }`;
-      const { data } = await axios(url);
-      setNoticias(data.articles);
-      setTotalNoticias(data.totalResults);
-      setPagina(1);
-    };
-    if (busqueda.length > 1) {
-      consultarAPI();
-    }
-  }, [busqueda]);
+  // useEffect(() => {
+  //   const consultarAPI = async () => {
+  //     const url = `https://newsapi.org/v2/everything?q=${busqueda}&pageSize=9&apiKey=${
+  //       import.meta.env.VITE_API_KEY
+  //     }`;
+  //     const { data } = await axios(url);
+  //     setNoticias(data.articles);
+  //     setTotalNoticias(data.totalResults);
+  //     setPagina(1);
+  //   };
+  //   if (busqueda.length > 1) {
+  //     consultarAPI();
+  //   }
+  // }, [busqueda]);
 
   const handleChangeCategoria = (e) => {
     setCategoria(e.target.value);
