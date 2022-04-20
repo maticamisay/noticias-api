b506342eaed2ecf260c14be84dd92f5a57a6b321
maticamisay <maticamisay99@hotmail.com>
2022-04-19 21:07:28 -0300

    volvemos a la api inicial

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -3,45 +3,47 @@ import { Button, Grid, TextField } from "@mui/material";
 import useNoticias from "../hooks/useNoticias";
 
 const Buscador = () => {
-  const [buscador, setBuscador] = useState("");
-  const { handleChangeBusqueda } = useNoticias();
+  const { busqueda, setBusqueda } = useNoticias();
   const handleChangeBuscador = (e) => {
-    setBuscador(e.target.value);
-    // e.target.value='hola'
+    e.preventDefault();
+    setBusqueda(searchText);
+    e.target.reset();
   };
-  const realizarBusqueda = () =>{
-    handleChangeBusqueda(buscador)
-    
-  }
+  const [searchText, setSearchText] = useState("");
+  const handleChangeSearchBar = (e) => {
+    setSearchText(e.target.value);
+  };
   return (
     <>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems={"center"}
-        spacing={1}
-      >
-        <Grid item xs>
-          <TextField
-            fullWidth
-            id="outlined-basic"
-            label="Ingresa tu búsqueda"
-            variant="outlined"
-            onChange={handleChangeBuscador}
-            // value='busqueda'
-          />
+      <form onSubmit={handleChangeBuscador}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems={"center"}
+          spacing={1}
+        >
+          <Grid item xs>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Ingresa tu búsqueda"
+              variant="outlined"
+              name="buscando"
+              onChange={handleChangeSearchBar}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              sx={{ padding: "14px 14px" }}
+              type="submit"
+            >
+              Buscar
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            variant="outlined"
-            sx={{ padding: "14px 14px" }}
-            onClick={()=>handleChangeBusqueda(buscador)}
-          >
-            Buscar
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </>
   );
 };
--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,78 +1,12 @@
 import axios from "axios";
 import { useState, useEffect, createContext } from "react";
-
 const NoticiasContext = createContext();
-
 const NoticiasProvider = ({ children }) => {
   const [categoria, setCategoria] = useState("general");
   const [noticias, setNoticias] = useState([]);
   const [busqueda, setBusqueda] = useState("");
   const [pagina, setPagina] = useState(1);
   const [totalNoticias, setTotalNoticias] = useState(0);
-
-  // const options = {
-  //   method: "GET",
-  //   url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI",
-  //   params: {
-  //     pageNumber: "1",
-  //     pageSize: "10",
-  //     withThumbnails: "false",
-  //     location: "us",
-  //   },
-  //   headers: {
-  //     "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-  //     "X-RapidAPI-Key": "d05773b8ddmsh6957041315b86a0p125a35jsn81d6227b7196",
-  //   },
-  // };
-
-  // axios
-  //   .request(options)
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
-  // useEffect(() => {
-  //   const options = {
-  //     method: "GET",
-  //     url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI",
-  //     params: {
-  //       pageNumber: "1",
-  //       pageSize: "10",
-  //       withThumbnails: "false",
-  //       location: "us",
-  //     },
-  //     headers: {
-  //       "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-  //       "X-RapidAPI-Key": "d05773b8ddmsh6957041315b86a0p125a35jsn81d6227b7196",
-  //     },
-  //   };
-  //   axios
-  //     .request(options)
-  //     .then(function (response) {
-  //       // console.log(response.data.value);
-  //       const data = response.data.value;
-  //       console.log(data);
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // }, [categoria]);
-  useEffect(() => {
-    const consultarAPI = async () => {
-      const url = `https://api.themoviedb.org/3/movie/popular?api_key=8d5c01130be75737905819a7317547e3&language=en-US&page=1`;
-      const img = `https://api.themoviedb.org/3/movie/634649/images?api_key=8d5c01130be75737905819a7317547e3&language=en-US`;
-      const { data } = await axios(url);
-      const { imgdata } = await axios(img);
-
-      console.log(imgdata);
-      // setNoticias(data.articles);
-      // setTotalNoticias(data.totalResults);
-      // setPagina(1);
-    };
-    consultarAPI();
-  }, []);
   useEffect(() => {
     const consultarAPI = async () => {
       const url = `https://newsapi.org/v2/top-headlines?pageSize=9&country=ar&category=${categoria}&apiKey=${
@@ -85,45 +19,36 @@ const NoticiasProvider = ({ children }) => {
     };
     consultarAPI();
   }, [categoria]);
-
-  // useEffect(() => {
-  //   const consultarAPI = async () => {
-  //     const url = `https://newsapi.org/v2/top-headlines?country=ar&pageSize=9&page=${pagina}&category=${categoria}&apiKey=${
-  //       import.meta.env.VITE_API_KEY
-  //     }`;
-
-  //     const { data } = await axios(url);
-  //     setNoticias(data.articles);
-  //     setTotalNoticias(data.totalResults);
-  //   };
-  //   consultarAPI();
-  // }, [pagina]);
-
-  // useEffect(() => {
-  //   const consultarAPI = async () => {
-  //     const url = `https://newsapi.org/v2/everything?q=${busqueda}&pageSize=9&apiKey=${
-  //       import.meta.env.VITE_API_KEY
-  //     }`;
-  //     const { data } = await axios(url);
-  //     setNoticias(data.articles);
-  //     setTotalNoticias(data.totalResults);
-  //     setPagina(1);
-  //   };
-  //   if (busqueda.length > 1) {
-  //     consultarAPI();
-  //   }
-  // }, [busqueda]);
-
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&pageSize=9&page=${pagina}&category=${categoria}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+    };
+    consultarAPI();
+  }, [pagina]);
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/everything?q=${busqueda}&pageSize=9&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+      setPagina(1);
+    };
+    if (busqueda.length > 1) {
+      consultarAPI();
+    }
+  }, [busqueda]);
   const handleChangeCategoria = (e) => {
     setCategoria(e.target.value);
   };
-
   const handleChangePagina = (e, valor) => {
     setPagina(valor);
-  };
-  const handleChangeBusqueda = (e) => {
-    setBusqueda(e);
-    // console.log(e);
   };
 
   return (
@@ -135,7 +60,8 @@ const NoticiasProvider = ({ children }) => {
         totalNoticias,
         handleChangePagina,
         pagina,
-        handleChangeBusqueda,
+        busqueda,
+        setBusqueda,
       }}
     >
       {children}
